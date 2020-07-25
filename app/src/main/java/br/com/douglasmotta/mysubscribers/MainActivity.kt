b23410bd3a58767cdaf2c387dc221395df67efde
Douglas Motta <>
2020-07-25 16:21:33 +0100

    Parte 01 - Criação do Projeto e Configuração da Navigação Principal

---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.douglasmotta.mysubscribers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
