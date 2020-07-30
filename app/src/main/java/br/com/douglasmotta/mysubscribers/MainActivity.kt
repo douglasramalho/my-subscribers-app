30dc258933eb051a5d8672d0655a87d903535e41
Douglas Motta <>
2020-07-30 18:28:41 +0100

    Parte 03 - Configuração da Toolbar

--- app/src/main/java/br/com/douglasmotta/mysubscribers/MainActivity.kt
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.douglasmotta.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        app_toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
