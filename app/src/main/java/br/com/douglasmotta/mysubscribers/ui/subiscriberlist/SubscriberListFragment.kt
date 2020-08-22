13411d9d296049f3dbe6253cf2b7faddb0f369f2
Douglas Motta <>
2020-08-22 12:16:22 +0100

    Parte 06 - Criação da Tela Listagem de Subscribers

---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subiscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,15 @@
+package br.com.douglasmotta.mysubscribers.ui.subiscriberlist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import br.com.douglasmotta.mysubscribers.R
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+    private lateinit var viewModel: SubscriberListViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subiscriberlist/SubscriberListViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.douglasmotta.mysubscribers.ui.subiscriberlist
+
+import androidx.lifecycle.ViewModel
+
+class SubscriberListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
