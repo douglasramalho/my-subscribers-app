dd2b841e5e6f46aa0cd006dc039fbe604dba1b94
Douglas Motta <>
2020-08-22 12:40:30 +0100

    Parte 07 - Criação do Adapter e ViewHolder da Listagem de Subscribers

---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subiscriberlist/SubscriberListAdapter.kt
@@ -0,0 +1,38 @@
+package br.com.douglasmotta.mysubscribers.ui.subiscriberlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.douglasmotta.mysubscribers.R
+import br.com.douglasmotta.mysubscribers.data.db.entity.SubscriberEntity
+import kotlinx.android.synthetic.main.subscriber_item.view.*
+
+class SubscriberListAdapter(
+    private val subscribers: List<SubscriberEntity>
+) : RecyclerView.Adapter<SubscriberListAdapter.SubscriberListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubscriberListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.subscriber_item, parent, false)
+
+        return SubscriberListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SubscriberListViewHolder, position: Int) {
+        holder.bindView(subscribers[position])
+    }
+
+    override fun getItemCount(): Int = subscribers.size
+
+    class SubscriberListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textViewSubscriberName: TextView = itemView.text_subscriber_name
+        private val textViewSubscriberEmail: TextView = itemView.text_subscriber_email
+
+        fun bindView(subscriber: SubscriberEntity) {
+            textViewSubscriberName.text = subscriber.name
+            textViewSubscriberEmail.text = subscriber.email
+        }
+    }
+}
--- app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subiscriberlist/SubscriberListFragment.kt
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subiscriberlist/SubscriberListFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import br.com.douglasmotta.mysubscribers.R
+import br.com.douglasmotta.mysubscribers.data.db.entity.SubscriberEntity
+import kotlinx.android.synthetic.main.subscriber_list_fragment.*
 
 class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
 
@@ -11,5 +13,17 @@ class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val subscriberListAdapter = SubscriberListAdapter(
+            listOf(
+                SubscriberEntity(1, "Douglas", "douglas@douglas.com"),
+                SubscriberEntity(2, "Diogo", "diogo@diogo.com")
+            )
+        )
+
+        with(recycler_subscribers) {
+            setHasFixedSize(true)
+            adapter = subscriberListAdapter
+        }
     }
 }
