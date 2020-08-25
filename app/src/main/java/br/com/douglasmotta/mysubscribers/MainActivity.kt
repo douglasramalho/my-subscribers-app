8d28eb179a3404f944c0298513d89d4e9fb7ff71
Douglas Motta <>
2020-08-25 22:05:03 +0100

    Parte 09 - Efeito de transição de tela e ajustes na barra de ação do aplicativo

--- app/src/main/java/br/com/douglasmotta/mysubscribers/MainActivity.kt
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/MainActivity.kt
@@ -2,20 +2,36 @@ package br.com.douglasmotta.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(app_toolbar)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        app_toolbar.setupWithNavController(navController, appBarConfiguration)
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, _, _ ->
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/extension/NavExtension.kt
@@ -0,0 +1,19 @@
+package br.com.douglasmotta.mysubscribers.extension
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import br.com.douglasmotta.mysubscribers.R
+
+private val slideLeftOptions = NavOptions.Builder()
+    .setEnterAnim(R.anim.slide_in_right)
+    .setExitAnim(R.anim.slide_out_left)
+    .setPopEnterAnim(R.anim.slide_in_left)
+    .setPopExitAnim(R.anim.slide_out_right)
+    .build()
+
+fun NavController.navigateWithAnimations(
+    destinationId: Int,
+    animation: NavOptions = slideLeftOptions
+) {
+    this.navigate(destinationId, null, animation)
+}
